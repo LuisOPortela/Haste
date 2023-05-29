cafb38630d04c129dc8fd556cd5b66303d1e8b1c
up201806607 <up201806607@fe.up.pt>
2023-05-29 20:23:53 +0100

    update

---
+++ src/my_package/src/client.cpp
@@ -0,0 +1,55 @@
+#include "rclcpp/rclcpp.hpp"
+#include "custom_interfaces/srv/scan_twi.hpp"
+
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char **argv)
+{
+  
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("Scan_Twi_client");
+  rclcpp::Client<custom_interfaces::srv::ScanTwi>::SharedPtr client =
+    node->create_client<custom_interfaces::srv::ScanTwi>("Scan_Twi");
+
+  auto request = std::make_shared<custom_interfaces::srv::ScanTwi::Request>();
+  //request->a = atoll(argv[1]);
+ 
+  std::string input;
+  while (rclcpp::ok()) {
+
+    std::cout << "Enter a Command (or 'q' to quit):";
+    std::getline(std::cin, input);
+
+    if(input == "q")
+      break;
+
+    request->command = input;
+   
+    while (!client->wait_for_service(1s)) { //process request
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+        return 0;
+      
+      }
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    }
+
+    auto result = client->async_send_request(request);
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(node, result) ==
+      rclcpp::FutureReturnCode::SUCCESS)
+    {
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Console answer: \n %s", result.get()->output.c_str() );
+    } else {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+    }
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/my_package/src/service.cpp
@@ -0,0 +1,53 @@
+#include "rclcpp/rclcpp.hpp"
+#include "custom_interfaces/srv/scan_twi.hpp"
+
+#include <memory>
+#include <cstdio>
+#include <iostream>
+#include <array>
+
+
+void Scan(const std::shared_ptr<custom_interfaces::srv::ScanTwi::Request> request,
+          std::shared_ptr<custom_interfaces::srv::ScanTwi::Response>      response)
+{
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request");
+  
+  std::string text = std::string("popo");
+  //response->output = text;
+  response->output= request->command;
+
+
+  //std::string executeCommand(const std::string& command) {
+  std::array<char, 128> buffer;
+  std::string result;
+
+  FILE* pipe=popen(request->command.c_str(), "r");
+  if (!pipe){
+    throw std::runtime_error("Failed to execute command.");
+
+  }
+
+  while (fgets(buffer.data(),buffer.size(),pipe)!= nullptr) {
+    result += buffer.data();
+  }
+
+  pclose(pipe);
+
+  response->output= result;
+}
+
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("Scan_Twi_server");
+
+  rclcpp::Service<custom_interfaces::srv::ScanTwi>::SharedPtr service =
+    node->create_service<custom_interfaces::srv::ScanTwi>("Scan_Twi", &Scan);
+
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to responde");
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+}
