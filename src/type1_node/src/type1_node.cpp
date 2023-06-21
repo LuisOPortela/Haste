3a7fe76fa580960c2681e7baaa8d4df72e62a6e7
up201806607 <up201806607@fe.up.pt>
2023-06-22 00:00:17 +0100

    i

--- src/type1_node/src/type1_node.cpp
+++ src/type1_node/src/type1_node.cpp
@@ -26,13 +26,21 @@ std::vector<int> max_value={};
 class Type1_Node : public rclcpp::Node
 {
   public:
-    Type1_Node()
-    : Node(name_), count_(0)
+    Type1_Node(const std::string &name)
+    : Node(name), count_(0)
     {
+      declare_parameter("name","type1_default");
+      declare_parameter("measure_frequency","500");
+      declare_parameter("number_of_values","1");
+
+      get_parameter("name",name_);
+      get_parameter("measure_frequency",measure_frequency_);
+      get_parameter("number_of_values",number_of_values_);
+      
       std::string aux_topic = "topic_";
       
       auto aux_timer =std::chrono::milliseconds(measure_frequency_);
-      publisher_ = this->create_publisher<std_msgs::msg::Int16>(aux_topic, 10);
+      publisher_ = this->create_publisher<std_msgs::msg::Int16>(aux_topic+name_, 10);
       timer_ = this->create_wall_timer(
       std::chrono::milliseconds(measure_frequency_), std::bind(&Type1_Node::timer_callback, this));
     
@@ -50,13 +58,14 @@ class Type1_Node : public rclcpp::Node
 
   private:
     //Params list
-    std::string name_="type1_default";
+    std::string name_;
     int measure_frequency_=500;
     //assuming number of values is always 1
     int number_of_values_=1;
     
     std::vector<int> max_value_={};
     
+
     void timer_callback()
     {
       auto message = std_msgs::msg::Int16();
@@ -88,7 +97,8 @@ class Type1_Node : public rclcpp::Node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Type1_Node>());
+  
+  rclcpp::spin(std::make_shared<Type1_Node>("Type"));
   rclcpp::shutdown();
   return 0;
 }
